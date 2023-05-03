b77e870e3288fc9cabfc5125ed6fde6a6193a835
tokaretin <tokaretin@gmail.com>
2023-05-03 23:40:08 +0200

    Добавьте файлы проекта.

---
+++ simple_code_12.1/simple_code_12.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    int number;
+    while (true) {
+        std::cout << "Input the number: ";
+        std::cin >> number;
+
+        if (number % 2 == 0) {
+            std::cout << "Четное число" << '\n';
+        } 
+        else {
+            std::cout << "Нечетное" << '\n';
+        }
+    }
+}
+
